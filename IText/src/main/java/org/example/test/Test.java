2e7db5a0810366f422928c2b0bc5d246edd0a2fb
LorenzoMaiani0 <lorenzo.maiani3@studio.unibo.it>
2023-09-14 10:06:33 +0200

    Add SceneController to switch scene in utils package and create Test class

---
+++ IText/src/main/java/org/example/test/Test.java
@@ -0,0 +1,11 @@
+package org.example.test;
+
+public class Test {
+    /**
+     * Class to execute automatic testing on the application
+     * @param args
+     */
+    public static void main(String[] args){
+
+    }
+}
